0e580ec3a99440d67b692fe6d24e9a16a22ca7a5
Kiran Kumar <kirankumar@Kiran-Kumars-MacBook-Air.local>
2024-09-30 21:28:18 +0530

    new-frontend

---
+++ Zpay-frontend-Staging/Zpay/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './component/dashboard/sidebar/sidebar';
+
+function App() {
+  return (
+    <div className="App">
+     <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
